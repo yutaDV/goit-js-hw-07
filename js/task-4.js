a45b53ed8fd030b277bd3d09d7cb528911a5eaa0
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-05-22 17:30:29 +0300

    4-5 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,28 @@
+const form = document.querySelector(".login-form");
+
+
+form.addEventListener("submit", handleSubmit);
+
+
+function handleSubmit(event) {
+  event.preventDefault(); 
+
+  const form = event.currentTarget;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email,
+    password
+  };
+
+  console.log(formData);
+
+  form.reset();
+}
 
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+
+const widget = document.querySelector(".widget");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  event.preventDefault();
+
+  const newColor = getRandomHexColor();
+  console.log(newColor)
+  widget.style.backgroundColor = newColor;
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
